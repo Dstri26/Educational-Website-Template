1598e70f8fe966349d980fe1b5826df13791d8ff
Rupesh Chandra Mohanty <43889891+rupeshmohanty@users.noreply.github.com>
2020-11-04 23:19:20 +0530

    body text added for index page

---
+++ components/vision.php
@@ -0,0 +1,122 @@
+<style>
+    h3{
+
+        border-bottom: 1px solid black !important;
+        font-family: 'Alegreya SC', serif !important;
+
+    }
+
+    h5{
+
+        font-family: 'Alegreya SC', serif !important;
+        color: #172a3a;
+
+    }
+
+    .sidemenu{
+        border: 1px solid black !important;
+    }
+
+    summary{
+
+        padding: 5px;
+
+    }
+</style>
+
+<div class="container">
+    <!-- Heading -->
+    <h3 style = "color: #172a3a">Welcome to CET,Bhubaneswar</h3>
+
+
+    <!-- Row for vision and sidebar -->
+    <div class="row">
+        <!-- Vision -->
+        <div class="col-md-8">
+            <h5>Vision</h5>
+            <p>To emerge as a global leader in Technical Education</p>
+
+            <h5>Mission</h5>
+            <p>To generate on a sustainable basis quality technical human resources commensurate with the dynamic global scenario</p>
+
+            <h5>GRADUATE  ATTRIBUTES IN UG (ENGINEERING)</h5>
+            <div>  
+                <details>
+                    <summary>Engineering knowledge</summary>
+                    <p>
+                    Apply knowledge of mathematics, science, engineering fundamentals and an engineering specialization to the solution of complex engineering problems.
+                    </p>
+                </details>
+            
+                <details>
+                    <summary>Problem analysis</summary>
+                    <p>
+                    Identify, formulate, research literature and analyze complex engineering problems reaching substantiated conclusions using first principles of mathematics, natural sciences and engineering sciences.
+                    </p>
+                </details>
+
+                <details>
+                        <summary>Design & Development of Solutions</summary>
+                        <p>
+                        Design solutions for complex engineering problems and design system components or processes that meet specified needs with appropriate consideration for public health and safety, cultural, societal and environmental considerations.
+                        </p>
+                </details>
+                <details>
+                        <summary>Conduct investigations of complex problems</summary>
+                        <p>
+                        using research-based knowledge and research methods including design of experiments, analysis and interpretation of data and synthesis of information to provide valid conclusions.
+                        </p>
+                </details>
+                <details>
+                        <summary>Modern Tool Usage</summary>
+                        <p>
+                        Create, select and apply appropriate techniques, resources and modern engineering and IT tools including prediction and modelling to complex engineering activities with an under- standing of the limitations.
+                        </p>
+                </details>
+                <details>
+                        <summary>The Engineer and Society</summary>
+                        <p>
+                        Apply reasoning informed by contextual knowledge to assess societal, health, safety, legal and cultural issues and the consequent responsibilities relevant to professional engineering practice.
+                        </p>
+                </details>
+                <details>
+                        <summary>Environment and Sustainability</summary>
+                        <p>Understand the impact of professional engineering solutions in societal and environmental contexts and demonstrate knowledge of and need for sustainable development.</p>
+                </details>
+                <details>
+                        <summary>Ethics</summary>
+                        <p>Apply ethical principles and commit to professional ethics and responsibilities and norms of engineering practice.</p>
+                </details>
+                <details>
+                        <summary>Individual and Team Work</summary>
+                        <p>Function effectively as an individual, and as a member or leader in diverse teams and in multi disciplinary settings.</p>
+                </details>
+                <details>
+                        <summary>Communication</summary>
+                        <p>Communicate effectively on complex engineering activities with the engineering community and with society at large, such as being able to comprehend and write effective reports and design documentation, make effective presentations and give and receive clear instructions.</p>
+                </details>
+                <details>
+                        <summary>Life-long Learning</summary>
+                        <p>Recognize the need for and have the preparation and ability to engage in independent and life- long learning in the broadest context of technological change.</p>
+                </details>
+                <details>
+                        <summary>Project Management and Finance</summary>
+                        <p>Demonstrate knowledge and understanding of engineering and management principles and apply these to one’s own work, as a member and leader in a team, to manage projects and in multidisciplinary environments.</p>
+                </details>
+            </div><br/>
+            <p>
+            The College of Engineering & Technology, Bhubaneswar was established by the Government of Odisha in 1981 to meet the growing technical man power need in the State. It was a Constituent College of the Odisha University of Agriculture & Technology, Bhubaneswar since inception. After creation of a Technical University for Odisha State, the College has become a Constituent College of Biju Patnaik University of Technology (BPUT), Odisha with effect from 09th July, 2002 as per section-37(1) of BPUT Act, 2002. .
+
+ 
+
+The College is located in the Techno-campus (an area of more than 100 Acres) at Kalinga Nagar, Bhubaneswar about 2.0 Kms. away from Khandagiri-Udayagiri caves. . . <a href = "./about.php">more</a>
+            </p>
+        </div>
+        <!-- Sidebar -->
+        <div class="col-md-4">
+            <div class = "sidemenu">
+                This space is for the sidemenu<br/><br/>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    include "components/common/header.php";
+?>
+<br/>
+<?php
+    include "components/vision.php";
+?>
