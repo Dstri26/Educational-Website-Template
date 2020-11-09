e2f601b32255ae36d4d03875644178710c1c387c
Rupesh Chandra Mohanty <43889891+rupeshmohanty@users.noreply.github.com>
2020-11-10 00:39:52 +0530

    Department page half done

---
+++ department.php
@@ -0,0 +1,107 @@
+<style>
+    h3{
+
+        border-bottom: 1px solid black !important;
+        font-family: 'Alegreya SC', serif !important;
+
+    }
+    h5{
+
+        font-family: 'Alegreya SC', serif !important;
+        color: #172a3a;
+
+    }
+    summary{
+
+padding: 5px;
+
+    }
+</style>
+<?php
+    include "components/common/header.php";
+?>
+<?php
+    include "components/common/navbar.php";
+?>
+<section style="background:url('assets/img/visionbg.jpg');background-size:cover;">
+    <div class="container">
+        <!-- Heading -->
+        <h3 style = "color: #172a3a">Department of Instrumentation & Electronics Engineering (NBA Accredited)</h3>
+
+        <div class="col-md-7">
+            <h5>Vision</h5>
+            <p>To achieve excellent standards of education by keeping pace with cutting edge technologies in the field of Instrumentation and Electronics Engineering.</p>
+
+            <h5>Mission</h5>
+            <ul>
+                <li>
+                    To generate technical manpower meeting the dynamically changing scenario of industry, academic and start-ups.
+                </li>
+                <li>
+                    To install a sense of entrepreneurship among students to be innovative for social and industrial needs.
+                </li>
+                <li>
+                    To produce human beings having a strong value of ethics contributing positively to the society.
+                </li>
+            </ul>
+
+            <h5>Programme Educational Objectives (PEOs)</h5>
+            <div>
+                <details>
+                    <summary>PEO 1</summary>
+                    <p>
+                        Excel in professional career and/or higher education by acquiring knowledge in mathematical computing and engineering principles.
+                    </p>
+                </details>
+                <details>
+                    <summary>PEO 2</summary>
+                    <p>
+                    Analyze real life problems, design computing systems appropriate to its solutions those are technically sound, economically feasible and socially acceptable.
+                    </p>
+                </details>
+                <details>
+                    <summary>PEO 1</summary>
+                    <p>
+                    Exhibit professionalism, ethical attitude, communication skills, team work in their profession and adapt to current trends by engaging in life long learning.
+                    </p>
+                </details>
+            </div>
+
+            <h5>Program Outcomes (POs)</h5>
+            <ol>
+                <li>
+                    Acquiring sufficient knowledge on fundamentals of engineering sciences, applied mathematics and computational aspects and to apply the knowledge gained on laboratory experiments.
+                </li>
+                <li>
+                    Motivated with an interest to undergo higher studies in India or other countries.
+                </li>
+                <li>
+                Ability to design construct and test application oriented circuits and systems related analog/digital/mixed signal based electronics and telecommunication engineering.
+                </li>
+                <li>
+                Developing skills to carry out experiments listed in syllabus and to implement individual / group project.
+                </li>
+                <li>
+                Learning professional, managerial, ethical and environmental based principles to become good citizen of our nation.
+                </li>
+                <li>
+                Acquiring required broader knowledge on interdisciplinary engineering disciplines.
+                </li>
+                <li>
+                Enabling to execute team work without any disparity based on gender, caste, social background, etc.
+                </li>
+                <li>
+                Applying technical English communications skills and professional knowledge to earn job opportunities in leading organizations and presenting technical papers in the National / International Conferences, Journals, Workshops and Symposia.
+                </li>
+            </ol>
+
+            <p>
+            The Department of Instrumentation and Electronics Engineering was established in 1992. It is equipped with sophisticated laboratories and well qualified faculty. The students are made adept in the sessional at the microprocessors and pc lab, process control and instrumentation and device lab. These labs provide facilities for microprocessors and computer based temperature, pressure, flow and level. Control Engg. Lab adds to the further expertise in electrical, mechanical and electromechanical application based experiments. The dept. also possesses an in house computer hardware lab& PC lab. It has many 8bit/12bit/32bit processor based computer that are connected in the both Linux and Windows NT operating system environment. Other software package like PSPICE, MATLAB, VISUAL STUDIO, JAVA etc. are used for computer aided projects. These help the student in keeping pace with the recent developments in the field of information since and engg. And at the same time enhancing and strengthening their knowledge by designing packages required to solve real life problem. The faculty specializes in the field of process control & instrumentation, telecommunication engg. Digital system, automation engineering, biomedical instrumentation, artificial intelligence, microelectronics, electronic communication system etc.
+            </p>
+
+        </div>
+    </div>
+</section>
+<?php
+    include "components/common/footer.php";
+?>
