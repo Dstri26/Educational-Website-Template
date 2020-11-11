07187672aff62e0ba4aff5f8cf381ab7dcd9dd6d
Rupesh Chandra Mohanty <43889891+rupeshmohanty@users.noreply.github.com>
2020-11-11 16:42:26 +0530

    Tequip page added

--- components/common/navbar.php
+++ components/common/navbar.php
@@ -3,13 +3,13 @@
         <div class="container"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navcol-1">
                 <ul class="nav navbar-nav mx-auto">
-                    <li class="nav-item"><a class="nav-link" href="#" style="font-size: 25px;font-family: 'Alegreya SC', serif;color: #fff;">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php" style="font-size: 25px;font-family: 'Alegreya SC', serif;color: #fff;">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="#" style="font-size: 25px;font-family: 'Alegreya SC', serif;color: #fff;">About CET</a></li>
                     <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#" style="font-size: 25px;font-family: 'Alegreya SC', serif;color: #fff;">Departments</a>
                         <div class="dropdown-menu"><a class="dropdown-item" href="#">First Item</a><a class="dropdown-item" href="#">Second Item</a><a class="dropdown-item" href="#">Third Item</a></div>
                     </li>
-                    <li class="nav-item"><a class="nav-link" href="#" style="font-size: 25px;font-family: 'Alegreya SC', serif;color: #fff;">Faculty</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#" style="font-size: 25px;font-family: 'Alegreya SC', serif;color: #fff;">TEQUIP</a></li>
+                    <li class="nav-item"><a class="nav-link" href="faculty.php" style="font-size: 25px;font-family: 'Alegreya SC', serif;color: #fff;">Faculty</a></li>
+                    <li class="nav-item"><a class="nav-link" href="tequip.php" style="font-size: 25px;font-family: 'Alegreya SC', serif;color: #fff;">TEQUIP</a></li>
                     <li class="nav-item"><a class="nav-link" href="#" style="font-size: 25px;font-family: 'Alegreya SC', serif;color: #fff;">Autonomous</a></li>
                 </ul>
             </div>
